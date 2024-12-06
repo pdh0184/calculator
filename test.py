4af741418ffc03a562f166dead08ac1b8cab2cd4
pdh0184 <pdh0184@naver.com>
2024-12-06 16:55:07 +0900

    Test.py 변경

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 class TEST():
     
     def test1():
-        print('Test1 , PRINT!')
+        print('Test1, PRINT!')
+
+    def test2():
+        print('Test2, PRINT!')
 
 TEST.test1()
+TEST.test2()
